a5b34c56cdd600aa239794488ca21613465d1f94
John Shaughnessy <john@johnshaughnessy.com>
2022-06-25 22:48:45 -0400

    Org setup

--- emacs/.doom.d/config-org.el
+++ emacs/.doom.d/config-org.el
@@ -1,8 +1,9 @@
 (setq src-me       (file-truename "/home/john/src/me/"))
 (setq src-me-notes (file-truename "/home/john/src/me/notes/"))
 
-(setq prefix
-      "#+Author: John Shaughnessy\n")
+;; TODO: Use author prefix
+(setq author-prefix "#+Author: John Shaughnessy\n")
+
 (setq template-note
   '("n" "note" plain
     (file (lambda () (expand-file-name (format-time-string "%Y-%m-%d-%H-%M-%S.org") src-me-notes)))
@@ -11,19 +12,37 @@
     )
   )
 
+(setq prefix-key-habit
+      '("h" "habit"))
+
+(setq foo
+  (expand-file-name "org-mode-templates/habit-wake" src-me-notes))
+
+(setq template-habit-wake
+  '("hw" "wake" plain
+    (file (lambda () (expand-file-name (format-time-string "%Y-%m-%d-%H-%M-%S.org") src-me-notes)))
+    (file "~/src/me/org-mode-templates/habit-wake")
+    :unnarrowed t
+    )
+  )
+
 (setq org-agenda-files (list src-me src-me-notes))
-(setq org-capture-templates (list template-note))
+(setq org-capture-templates (list template-note prefix-key-habit template-habit-wake))
 (setq org-tag-alist '(("note")
                       ("project")
+                      ("question")
+                      ("diary")
                       ("todo")
                       ("done")
-                      ("question")
                       ("goal")
                       ("failed")
                       ("weekly")
                       ("mind")
                       ("body")
                       ("soul")
+                      ("habit")
+                      ("wake")
+                      ("inspire")
                       ))
 
 (setq org-agenda-prefix-format
