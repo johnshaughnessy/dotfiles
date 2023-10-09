734082b013e58faef95a5911a3980c6ff3b9f726
John Shaughnessy <john@johnshaughnessy.com>
2023-10-09 13:27:04 -0400

    Fix screenshot filename generator
    After the 8th screenshot, something went wrong:
    
    ~/.config/i3/generate_next_filename.sh
    /home/john/.config/i3/generate_next_filename.sh: line 10: 008: value too great for base (error token is "008")
    /home/john/src/blog/blog/static/image/the_book_of_why/screenshot_.png
    
    The issue arises from Bash interpreting numbers with leading zeros as octal values. When the script attempts to increment the value 008, it fails because 8 is not a valid digit in base 8 (octal).
    
    A straightforward solution is to ensure that the script treats these numbers as decimal by specifying the base or by stripping leading zeros before performing arithmetic operations.

--- i3/.config/i3/generate_next_filename.sh
+++ i3/.config/i3/generate_next_filename.sh
@@ -3,11 +3,16 @@
 # Directory to scan
 dir="/home/john/src/blog/blog/static/image/the_book_of_why"
 
-# Get the highest numbered file
-highest_number=$(find "$dir" -type f -name 'screenshot_*.png' | grep -oP '(?<=screenshot_)\d+(?=.png)' | sort -n | tail -1)
+# Get the highest numbered file, and ensure the number is treated as decimal by stripping leading zeros
+highest_number=$(find "$dir" -type f -name 'screenshot_*.png' | grep -oP '(?<=screenshot_)\d+(?=.png)' | sort -n | tail -1 | sed 's/^0*//')
+
+# If no files were found, start at 1
+if [ -z "$highest_number" ]; then
+    highest_number=0
+fi
 
 # Increment the number for the next filename
-next_number=$(printf "%03d" $((highest_number + 1)))
+next_number=$(printf "%03d" $((10#$highest_number + 1)))
 
 # Generate the next filename
 next_filename="screenshot_$next_number.png"
